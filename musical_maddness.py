6099bd0e9634c202bb697f61cbfc3482c28c7fce
telljay <toellja2221@gmail.com>
2024-09-23 11:45:26 -0500

    Beginning stages of development and fighting a losing battle with the spotify API

---
+++ musical_maddness.py
@@ -0,0 +1,22 @@
+import sqlite3
+from sqlite3 import Error
+
+import sys
+
+from flask import Flask
+from flask import request
+from flask import current_app
+from flask import abort
+app = Flask(__name__)
+import json
+def opening_screen():
+    return "Welcome to Advanced Musical Understanding\nPlease Log into your Spotify Account to Continue"
+#--
+def approve_login():
+    
+    return False
+#--
+def main():
+
+    return 0
+    
