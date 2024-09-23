ff839773c896516da0d0bc3338d116b2fa8c81db
telljay <toellja2221@gmail.com>
2024-09-23 12:30:11 -0500

    adding the spotipy library and beginning the fight between me and the Spotify API

--- musical_maddness.py
+++ musical_maddness.py
@@ -1,3 +1,5 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import sqlite3
 from sqlite3 import Error
 
@@ -9,6 +11,11 @@ from flask import current_app
 from flask import abort
 app = Flask(__name__)
 import json
+
+SPOTIPY_CLIENT_ID = 'e3d6ae52792d4f6bb286ef14c6ee270c'
+SPOTIPY_CLIENT_SECRET = '6214d65c3e454af6aa92c0ab49d14915'
+SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:5000/'
+
 def opening_screen():
     return "Welcome to Advanced Musical Understanding\nPlease Log into your Spotify Account to Continue"
 #--
