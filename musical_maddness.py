1f8dfb6d0c86219b297a2aae8572d6c29ca32390
telljay <toellja2221@gmail.com>
2024-09-27 11:30:36 -0500

    Continued the battle with the Spotify Api and finally got to the point of opening the webpage with only the opening screen text avalible

--- musical_maddness.py
+++ musical_maddness.py
@@ -1,21 +1,28 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+#--
 import sqlite3
 from sqlite3 import Error
-
+#--
 import sys
-
+#--
 from flask import Flask
 from flask import request
 from flask import current_app
 from flask import abort
 app = Flask(__name__)
+#--
 import json
-
+#--
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="e3d6ae52792d4f6bb286ef14c6ee270c",
+client_secret="6214d65c3e454af6aa92c0ab49d14915",
+redirect_uri="http://127.0.0.1:5000/",
+scope="user-library-read"))
+#--
 SPOTIPY_CLIENT_ID = 'e3d6ae52792d4f6bb286ef14c6ee270c'
 SPOTIPY_CLIENT_SECRET = '6214d65c3e454af6aa92c0ab49d14915'
 SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:5000/'
-
+@app.route("/")
 def opening_screen():
     return "Welcome to Advanced Musical Understanding\nPlease Log into your Spotify Account to Continue"
 #--
@@ -26,4 +33,5 @@ def approve_login():
 def main():
 
     return 0
-    
+#--
+main()
