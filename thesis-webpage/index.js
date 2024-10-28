a8eff8f5f2ba6acfb61ddddddc955a57c99eb113
telljay <toellja2221@gmail.com>
2024-10-28 11:46:16 -0500

    Creating a searchbar slowly but surely

--- thesis-webpage/index.js
+++ thesis-webpage/index.js
@@ -9,3 +9,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+app.get('/search',(req, res) =>{
+  const query =  req.query.q;
+  
+  res.send('Please enter the name of the artist you would like to rank:')
+});
