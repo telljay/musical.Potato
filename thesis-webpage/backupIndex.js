f335349b98e75701b1fc72d287d4ed2d23ccc18c
telljay <toellja2221@gmail.com>
2025-03-28 12:56:22 -0500

    Made everything look nicer using Handlebars and improved some functionality

--- thesis-webpage/backupIndex.js
+++ thesis-webpage/backupIndex.js
@@ -7,6 +7,10 @@ const port = 1989;
 const SpotifyWebApi = require('spotify-web-api-node');
 const DBAbstraction = require("./DBAbstraction");
 const db = new DBAbstraction(path.join(__dirname,'data',"musicalPotato.sqlite"));
+const handlebars = require('express-handlebars').create({}); 
+
+app.engine('handlebars', handlebars.engine); 
+app.set('view engine', 'handlebars');
 
 app.use(morgan('dev'));
 //Functions to create SQL queries
@@ -57,21 +61,22 @@ db.init()
 //Serve the homepage with authorizeURL
 
 app.get('/', (req, res) => {
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Home</title>
-        </head>
-        <body>
-            <h1>Welcome to Advanced Musical Understanding</h1>
-            <h3>Please login to your Spotify account</h3>
-            <a id="loginLink" href="${authorizeURL}">Login with Spotify</a>
-        </body>
-        </html>
-    `);
+    res.render('home', {URL: authorizeURL})
+    // res.send(`
+    //     <!DOCTYPE html>
+    //     <html lang="en">
+    //     <head>
+    //         <meta charset="UTF-8">
+    //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //         <title>Home</title>
+    //     </head>
+    //     <body>
+    //         <h1>Welcome to Advanced Musical Understanding</h1>
+    //         <h3>Please login to your Spotify account</h3>
+    //         <a id="loginLink" href="${authorizeURL}">Login with Spotify</a>
+    //     </body>
+    //     </html>
+    // `);
 });
 
 app.get('/auth', (req, res) => {
@@ -98,7 +103,10 @@ function QueSearchResult(searchWord) {
         .then(function(data) {
             let artistNames = [];
             data.body.artists.items.forEach(artist => {
-                artistNames.push(artist.name);
+                //artistNames[name] = artist.id;
+                let link = `http://localhost:1989/readytorank?id=${artist.id}&name=${artist.name}`
+                artistNames.push({name: artist.name, link: link});
+
                 artistNamesAndID[artist.name] = artist.id;
             });
             return artistNames;
@@ -109,35 +117,41 @@ function QueSearchResult(searchWord) {
 }
 
 app.get('/search', (req,res)=>{
-  res.sendFile(path.join(__dirname, 'public', 'search.html')); 
+    res.render('search');
+  //res.sendFile(path.join(__dirname, 'public', 'search.html')); 
 });
 
 app.get('/results', async (req, res) => {
     const query = req.query.q;
     console.log("Receive search", query);
-    const artistNames = await QueSearchResult(query);
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Results</title>
-        </head>
-        <body>
-            <h1>Results...</h1>
-            ${artistNames.map(name => `<a id = "artistResultsUrl" href = http://localhost:1989/readytorank?id=${artistNamesAndID[name]}&name=${name}>${name}</a>`).join('<h4></h4>')}
-        </body>
-        </html>
-    `);
+    let artists = await QueSearchResult(query);
+    console.log(artists);
+    res.render('results', {artists})
+
+    // res.send(`
+    //     <!DOCTYPE html>
+    //     <html lang="en">
+    //     <head>
+    //         <meta charset="UTF-8">
+    //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //         <title>Results</title>
+    //     </head>
+    //     <body>
+    //         <h1>Results...</h1>
+    //         ${artistNames.map(name => `<a id = "artistResultsUrl" href = http://localhost:1989/readytorank?id=${artistNamesAndID[name]}&name=${name}>${name}</a>`).join('<h4></h4>')}
+    //     </body>
+    //     </html>
+    // `);
 });
 var albumsandId = {};
-function GetAlbums(artistID){
+function GetAlbums(artistID,dbId){
     return spotifyApi.getArtistAlbums(artistID)
         .then(function(data){
             let quedAlbums = []
             data.body.items.forEach(album => {
-                quedAlbums.push(album.name);
+                let link = `http://localhost:1989/ranking?albumId=${album.id}&artistDBID=${dbId}`
+                quedAlbums.push({Title: album.name, Link: link});
+                //quedAlbums.push(album.name);
                 albumsandId[album.name] = album.id;
             });
             return quedAlbums;
@@ -152,35 +166,37 @@ app.get("/readytorank", async (req, res) => {
     db.insertArtist(req.query.id, req.query.name)
         .then((artistId) => {
             console.log(`Artist ID: ${artistId}`); // Should log the correct artist ID
+            artistID = artistId;
         })
         .catch((err) => {
             console.error(`Error: ${err.message}`);
         });
-    const albums = await GetAlbums(req.query.id);
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Ranking Page</title>
-        </head>
-        <body>
-            <h1>Select the Album to begin with...</h1>
-            <ul id="albumList">
-                ${albums.map(album => `<a id = "albumNameId" href = http://localhost:1989/ranking/?albumId=${albumsandId[album]}&artistDBID=${artistID}>${album}</a> <h3/>`).join('')}
-            </ul>
-        </body>
-        </html>
-    `);
+    const albums = await GetAlbums(req.query.id,artistID);
+    res.render('albums', {albums});
+    // res.send(`
+    //     <!DOCTYPE html>
+    //     <html lang="en">
+    //     <head>
+    //         <meta charset="UTF-8">
+    //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //         <title>Ranking Page</title>
+    //     </head>
+    //     <body>
+    //         <h1>Select the Album to begin with...</h1>
+    //         <ul id="albumList">
+    //             ${albums.map(album => `<a id = "albumNameId" href = http://localhost:1989/ranking?albumId=${albumsandId[album]}&artistDBID=${artistID}>${album}</a> <h3/>`).join('')}
+    //         </ul>
+    //     </body>
+    //     </html>
+    // `);
 });
 
-app.use('ranking/:albumID',async (req,res)=>{
+app.get('/ranking',async (req,res)=>{
     let albuminfo = await spotifyApi.getAlbum(req.query.albumID)
     await db.insertAlbum(req.query.albumID,albuminfo.name, req.query.artistDBID).then(console.log("success"))
 })
 
-app.use('ranking/:albumID/:songID', (req,res)=>{
+app.get('/ranking/:albumID/:songID', (req,res)=>{
 
 })
 
